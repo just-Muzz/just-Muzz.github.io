ae6e2e27e02f94829ea57669367448bcd6aafeab
just-Muzz <idrisking759@gmail.com>
2023-06-04 19:32:02 +0100

    edited

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -7,7 +7,7 @@ import TrackVisibility from 'react-on-screen';
 const Banner = () => {
   const [loopNum, setLoopNum] = useState(0);
   const [isDeleting, setIsDeleting] = useState(false);
-  const toRotate = ["Nigerian", "Muslim", "14", "Frontend dev", "React JS"]
+  const toRotate = ["Nigerian", "Muslim", "15", "Frontend dev", "React JS"]
   const [text, setText] = useState('');
   const [delta, setDelta] = useState(300 - Math.random() * 100)
   const [index, setIndex] = useState(1);
@@ -55,7 +55,7 @@ const Banner = () => {
               {({ isVisible }) =>
                 <div className={isVisible ? "animate__animated animate__slideInLeft" : ""}>
                   <span className="tagline">Welcome to my portfolio</span>
-                  <h1>{"I am just-Muzz"} <span className="txt-rotate" data-rotate='["Nigerian", "Muslim", "14", "Frontend dev", "React JS"]'><span className="wrap">{text}</span></span></h1>
+                  <h1>{"I am just-Muzz"} <span className="txt-rotate" data-rotate='["Nigerian", "Muslim", "15", "Frontend dev", "React JS"]'><span className="wrap">{text}</span></span></h1>
                   <p>{toRotate[0]} || {toRotate[1]} || {toRotate[2]} || {toRotate[3]} || {toRotate[4]}</p>
                   <button>Let's Connect <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width="25" height="25" fill="currentColor"><path fillRule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5z"></path></svg></button>
                 </div>}
